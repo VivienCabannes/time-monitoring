7cd6ae4d2e73e6c326f5c56ba39733ddf95ae95b
VivienCabannes <vivien.cabannes@gmail.com>
2022-01-12 12:11:57 +0100

    Unit tests: check time report

--- tests/test_stop.py
+++ tests/test_stop.py
@@ -1,15 +1,21 @@
 """Testing for reports"""
 import csv
+from datetime import datetime
 from subprocess import Popen, PIPE
-from time_monitor.config import BUFFER_FILE, REPORT_FILE
+from time_monitor.config import (
+    BUFFER_FILE,
+    REPORT_FILE,
+    DATE_FORMAT,
+)
 # try:
-#     from time_monitor.config import BUFFER_FILE, REPORT_FILE
+#     from time_monitor.config import BUFFER_FILE, REPORT_FILE, DATE_FORMAT
 # except ModuleNotFoundError as module_err:
 #     print(module_err)
 #     from pathlib import Path
 #     BUFFER_PATH = Path.home() / '.time-monitoring'
 #     BUFFER_FILE = BUFFER_PATH / '.activity'
 #     REPORT_FILE = BUFFER_PATH / '.current_report.csv'
+#     DATE_FORMAT = '%Y-%m-%d %H:%M'
 
 ACTIVITY = 'my activity'
 DATE = '2021-01-01 10:00'
@@ -39,11 +45,18 @@ def test_stop():
     with open(BUFFER_FILE, 'w') as f:
         f.writelines(tmp)
     process = Popen(['stop'], stdout=PIPE, stderr=PIPE)
+    # record end time
+    tf = datetime.utcnow()
+    dt = tf - datetime.strptime(DATE, DATE_FORMAT)
+    length = dt.days * 1440 + dt.seconds // 60
+    tf = tf.strftime(DATE_FORMAT)
+    # get process execution variable
     (out, err) = process.communicate()
     assert out == b''
     assert err == b''
     with open(REPORT_FILE, 'r', newline='') as f:
         rows = list(csv.reader(f, delimiter=','))
+    assert abs(length - int(rows[-1][3])) < 1
     assert rows[-1] == [
         ACTIVITY, DATE, rows[-1][2], rows[-1][3], ''
     ]
@@ -58,11 +71,18 @@ def test_stop_message():
     with open(BUFFER_FILE, 'w') as f:
         f.writelines(tmp)
     process = Popen(['stop'], stdout=PIPE, stderr=PIPE)
+    # record end time
+    tf = datetime.utcnow()
+    dt = tf - datetime.strptime(DATE, DATE_FORMAT)
+    length = dt.days * 1440 + dt.seconds // 60
+    tf = tf.strftime(DATE_FORMAT)
+    # get process execution variable
     (out, err) = process.communicate()
     assert out == b''
     assert err == b''
     with open(REPORT_FILE, 'r', newline='') as f:
         rows = list(csv.reader(f, delimiter=','))
+    assert abs(length - int(rows[-1][3])) < 1
     assert rows[-1] == [
         ACTIVITY, DATE, rows[-1][2], rows[-1][3], MESSAGE
     ]
@@ -77,11 +97,18 @@ def test_stop_messages():
     with open(BUFFER_FILE, 'w') as f:
         f.writelines(tmp)
     process = Popen(['stop'], stdout=PIPE, stderr=PIPE)
+    # record end time
+    tf = datetime.utcnow()
+    dt = tf - datetime.strptime(DATE, DATE_FORMAT)
+    length = dt.days * 1440 + dt.seconds // 60
+    tf = tf.strftime(DATE_FORMAT)
+    # get process execution variable
     (out, err) = process.communicate()
     assert out == b''
     assert err == b''
     with open(REPORT_FILE, 'r', newline='') as f:
         rows = list(csv.reader(f, delimiter=','))
+    assert abs(length - int(rows[-1][3])) < 1
     assert rows[-1] == [
         ACTIVITY, DATE, rows[-1][2], rows[-1][3], MESSAGE + ' - ' + MESSAGE + ' - ' + MESSAGE
     ]
